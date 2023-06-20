4ce00989c6de08cee3b94285c2c90fc948c8e315
Estevão Soares <estevao.silva@4innovation.com.br>
2023-06-20 10:20:34 -0300

    first commit

---
+++ javinha.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var options = {};
+    var instances = M.Collapsible.init(elems, options);
+});
